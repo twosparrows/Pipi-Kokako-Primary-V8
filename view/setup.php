8de2944726be0224cd01c0e0ed1eabcadbad903e
Two Sparrows <web-projects@twosparrows.co.nz>
2024-05-30 12:11:59 +1200

    CSS to date (progressively getting CSS compiler working with rebuilt CSS Editor)

--- view/setup.php
+++ view/setup.php
@@ -4,14 +4,14 @@
 
 THEME SETUP
 
-Updated at 28/05/2024 2:10.00am UTC
+Updated at 30/05/2024 12:08.43am UTC
 
 Included via functions/site.php
 
 */
 
 
-$GLOBALS['updatedTimeStamp'] = 1716862200;
+$GLOBALS['updatedTimeStamp'] = 1717027723;
 
 // Development Modes
 $GLOBALS['kokakoDevelopmentMode'] = false;
@@ -22,7 +22,18 @@ $GLOBALS['additionalBodyClasses'] = "header-fixed";
 
 // Sections
 $GLOBALS['availableSectionTypes'] = array( // NB: Text section is always available
+     "call-to-action",
+     "centred-images",
+     "image-grid",
+     "latest-posts",
+     "multiple-columns",
+     "multiple-rows",
+     "page-banner",
+     "page-heading",
+     "slider",
+     "testimonials",
      "text",
+     "text-with-photo",
 );
 
 // JS Dependencies
@@ -37,6 +48,14 @@ $GLOBALS['widgetAreas'] = array(
 
 // Colour Schemes
 $GLOBALS['colourSchemes'] = array(
+     "colour-scheme-light" => array(
+          "description" => "Light Colour Scheme (Light background, dark text)",
+          "background"  => "#fff",
+     ),
+     "colour-scheme-dark" => array(
+          "description" => "Dark Colour Scheme (dark background, light text)",
+          "background"  => "#1a1a1a",
+     ),
 );
 
 // Logos
@@ -48,7 +67,7 @@ $GLOBALS['footerLogoWidthRetina'] = 510;
 // Functions
 
 function tsp_updated_timeStamp() { // Can also be called as a function if required, although higher overhead than a variable
-     return 1716862200;
+     return 1717027723;
 }
 
 ?>
