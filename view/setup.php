0a18fdc717ffa9c74ed1547aa7cea1c4916c5b9d
Two Sparrows <web-projects@twosparrows.co.nz>
2024-06-04 16:24:20 +1200

    CSS & JS now both compiling (some known issues documented in README that won't affect current dev work)

--- view/setup.php
+++ view/setup.php
@@ -4,14 +4,14 @@
 
 THEME SETUP
 
-Updated at 31/05/2024 4:29.28am UTC
+Updated at 04/06/2024 4:19.30am UTC
 
 Included via functions/site.php
 
 */
 
 
-$GLOBALS['updatedTimeStamp'] = 1717129768;
+$GLOBALS['updatedTimeStamp'] = 1717474770;
 
 // Development Modes
 $GLOBALS['kokakoDevelopmentMode'] = false;
@@ -31,13 +31,48 @@ $GLOBALS['availableSectionTypes'] = array( // NB: Text section is always availab
      "page-banner",
      "page-heading",
      "slider",
-     "testimonials",
      "text",
      "text-with-photo",
 );
 
 // JS Dependencies
-$GLOBALS['jsDependencies'] = array();
+$GLOBALS['jsDependencies'] = array(
+     "jquery" => array(
+          "source" => "//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+          "dependencies" => "",
+          "version" => "3.6.0",
+          "in_footer" => "1",
+          "user_logged_in" => "",
+     ),
+     "gsap" => array(
+          "source" => "//cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js",
+          "dependencies" => "",
+          "version" => "3.9.1",
+          "in_footer" => "1",
+          "user_logged_in" => "",
+     ),
+     "scrollMagic" => array(
+          "source" => "//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js",
+          "dependencies" => array("jquery", "gsap"),
+          "version" => "2.0.7",
+          "in_footer" => "1",
+          "user_logged_in" => "",
+     ),
+     "scrollMagicJquery" => array(
+          "source" => "//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/jquery.ScrollMagic.min.js",
+          "dependencies" => array("jquery", "gsap", "scrollMagic"),
+          "version" => "2.0.7",
+          "in_footer" => "1",
+          "user_logged_in" => "",
+     ),
+     "scrollMagicGsap" => array(
+          "source" => "//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js",
+          "dependencies" => array("jquery", "gsap", "scrollMagic", "scrollMagicJquery"),
+          "version" => "2.0.7",
+          "in_footer" => "1",
+          "user_logged_in" => "",
+     ),
+);
 
 // Widget areas
 $GLOBALS['widgetAreas'] = array(
@@ -48,22 +83,6 @@ $GLOBALS['widgetAreas'] = array(
 
 // Colour Schemes
 $GLOBALS['colourSchemes'] = array(
-     "colour-scheme-light" => array(
-          "description" => "Light Colour Scheme (Light background, dark text)",
-          "background"  => "#fff",
-     ),
-     "colour-scheme-dark" => array(
-          "description" => "Dark Colour Scheme (dark background, light text)",
-          "background"  => "#1a1a1a",
-     ),
-     "colour-scheme-foo" => array(
-          "description" => "Foo Colour Scheme (Foo background, light text)",
-          "background"  => "#f00",
-     ),
-     "colour-scheme-green" => array(
-          "description" => "Green Colour Scheme (Green background, dark text)",
-          "background"  => "#81d742",
-     ),
 );
 
 // Logos
@@ -75,7 +94,7 @@ $GLOBALS['footerLogoWidthRetina'] = 510;
 // Functions
 
 function tsp_updated_timeStamp() { // Can also be called as a function if required, although higher overhead than a variable
-     return 1717129768;
+     return 1717474770;
 }
 
 ?>
