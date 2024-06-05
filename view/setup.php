a1444a7f35bcf0ec911a8eba787bf2bfe317d21a
Two Sparrows <web-projects@twosparrows.co.nz>
2024-06-05 17:00:29 +1200

    Minor CSS update (some sections turned off).

--- view/setup.php
+++ view/setup.php
@@ -4,14 +4,14 @@
 
 THEME SETUP
 
-Updated at 04/06/2024 4:19.30am UTC
+Updated at 05/06/2024 1:59.15am UTC
 
 Included via functions/site.php
 
 */
 
 
-$GLOBALS['updatedTimeStamp'] = 1717474770;
+$GLOBALS['updatedTimeStamp'] = 1717552755;
 
 // Development Modes
 $GLOBALS['kokakoDevelopmentMode'] = false;
@@ -22,17 +22,7 @@ $GLOBALS['additionalBodyClasses'] = "header-fixed";
 
 // Sections
 $GLOBALS['availableSectionTypes'] = array( // NB: Text section is always available
-     "call-to-action",
-     "centred-images",
-     "image-grid",
-     "latest-posts",
-     "multiple-columns",
-     "multiple-rows",
-     "page-banner",
-     "page-heading",
-     "slider",
      "text",
-     "text-with-photo",
 );
 
 // JS Dependencies
@@ -83,6 +73,22 @@ $GLOBALS['widgetAreas'] = array(
 
 // Colour Schemes
 $GLOBALS['colourSchemes'] = array(
+     "colour-scheme-light" => array(
+          "description" => "Light Colour Scheme (Light background, dark text)",
+          "background"  => "#fff",
+     ),
+     "colour-scheme-dark" => array(
+          "description" => "Dark Colour Scheme (dark background, light text)",
+          "background"  => "#1a1a1a",
+     ),
+     "colour-scheme-foo" => array(
+          "description" => "Foo Colour Scheme (Foo background, light text)",
+          "background"  => "#f00",
+     ),
+     "colour-scheme-green" => array(
+          "description" => "Green Colour Scheme (Green background, dark text)",
+          "background"  => "#81d742",
+     ),
 );
 
 // Logos
@@ -94,7 +100,7 @@ $GLOBALS['footerLogoWidthRetina'] = 510;
 // Functions
 
 function tsp_updated_timeStamp() { // Can also be called as a function if required, although higher overhead than a variable
-     return 1717474770;
+     return 1717552755;
 }
 
 ?>
